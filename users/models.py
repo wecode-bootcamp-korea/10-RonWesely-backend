de7d419d34169762f31cfa475586e267c627eb3d
soomialexhwang <ifyouseeksoomi@gmail.com>
2020-07-22 22:06:51 +0900

    회원가입, 로그인 API models.py 주석 삭제 수정

--- users/models.py
+++ users/models.py
@@ -16,7 +16,6 @@ class Users(models.Model):
     on_notification = models.BooleanField(default = False)
     created_at = models.DateTimeField(auto_now_add =True)
     modified_at = models.DateTimeField(auto_now = True)
-   # survey = models.ManyToManyField(Surveys)
 
 class Meta:
     db_table = "users"
@@ -24,37 +23,14 @@ class Meta:
 def __str__(self):
     return self.userid
 
-#class Survey(models.Model):
-#
-#    SURVEY_CHOICES= (
-#        ('1', 'off_line'),
-#        ('2', 'on_line'),
-#        ('3', 'blog'),
-#        ('4', 'friends'),
-#        ('5', 'etc')
-#    )
-#
-#    choice = models.ChoiceField(("Survey"), max_length=80, choices=SURVEY_CHOICES)
-#
-#class Meta:
-#    db_table = "surveys"
-#
-#def __str__(self):
-#    return self.choice
-#
-#class Shipping(model.Model):
-#    user_id = models.ForeignKeyField(Users, on_delete=models.CASCADE)
-#    recipient = models.CharField(max_length=100)
-#    phone_no = models.CharField(max_length=50)
-#    address = models.CharField(max_length=50)
-#    address_detail = models.CharField(max_length=100)
-#    requirement = models.CharField(max_length=200)
-#    is_default = models.BooleanField(default=False)
-#   
-#class Meta:
-#    db_table = "shippings"
-#
-#def __str__(self):
-#    return self.recipient
+
+
+
+
+
+
+
+
+
 
 
