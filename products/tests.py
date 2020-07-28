192456bc5962bd96ef920adb47d457dab04ccdb3
Gonho LEE <sincerity410@gmail.com>
2020-07-28 16:06:28 +0900

    Fix products/tests.py, products/views.py

--- products/tests.py
+++ products/tests.py
@@ -1,19 +1,44 @@
-from django.test    import TestCase,Client
+import json
+
+from django.test    import (
+    TestCase,
+    Client
+)
+
+from .models        import (
+    Product,
+    Color,
+    ProductColor
+)
 
 class ProductTest(TestCase):
+    def setUp(self):
+        Product.objects.create(
+            name        = "선물세트",
+            description = "면도용품 + 기프트 카드"
+        )
+        Color.objects.create(
+            name = "미드나이트 네이비",
+        )
+        ProductColor.objects.create(
+            price      = "29800",
+            color_id   = Color.objects.get(name = "미드나이트 네이비").id,
+            product_id = Product.objects.get(id = 1).id
+        )
+
+    def tearDown(self):
+        Product.objects.all().delete()
+        Color.objects.all().delete()
+        ProductColor.objects.all().delete()
+
     def test_get_product_success_view(self):
         client   = Client()
-        response = self.client.get('products/1')
+        response = self.client.get('/product/1')
+
         self.assertEqual(response.status_code,200)
         self.assertEqual(response.json(), {
-                                            "Info": [
-                                                        {
-                                                            "name": "선물세트",
-                                                            "description": "면도용품 + 기프트 카드",
-                                                            "price": "29,800"
-                                                        }
-                                                    ]
-                                            }
-                                        )
-
-
+            "Info": [{
+                "name"        : "선물세트",
+                "description" : "면도용품 + 기프트 카드",
+                "price"       : "29,800"
+        }]})
--- products/views.py
+++ products/views.py
@@ -1,43 +1,48 @@
 from django.http        import JsonResponse
 from django.views       import View
 
-from products.models    import Product,BladeProduct,ProductColor,ProductSize
+from products.models    import (
+    Product,
+    BladeProduct,
+    ProductColor
+)
 
 class ProductDetail(View):
 
     def get(self,request,product_id):
         try:
             product = Product.objects.get(id=product_id)
+            print("product=",end=""), print(product)
 
             if product_id >= 1 and product_id < 3:
                 product_color   = ProductColor.objects.get(product_id=product_id,color_id=1)
                 price           = f'{int(product_color.price):,}'
-                product_info    =   [{
-                                        "name"        : product.name,
-                                        "description" : product.description,
-                                        "price"       : price
-                                    }]
+                product_info    = [{
+                    "name"        : product.name,
+                    "description" : product.description,
+                    "price"       : price
+                }]
                 return JsonResponse({'Info':product_info}, status=200)
 
             if product_id == 3:
                 blade_product   = BladeProduct.objects.get(product_id=product_id)
                 price           = f'{int(blade_product.price):,}'
-                product_info    =   [{
-                                        "name"        : product.name,
-                                        "description" : product.description,
-                                        "price"       : price
-                                    }]
+                product_info    = [{
+                    "name"        : product.name,
+                    "description" : product.description,
+                    "price"       : price
+                }]
                 return JsonResponse({'Info':product_info}, status=200)
 
             if product_id >= 4 and product_id < 6:
                 prefetch_product_size   = Product.objects.prefetch_related('productsize_set').get(id=product_id)
-                product_size_data       =   [{
-                                                'product_name'     : product.name,
-                                                'product_size'     : product_size.size.name,
-                                                'product_price'    : f'{int(product_size.price):,}',
-                                                'size_description' : product_size.size.description
-                                            } for product_size
-                                            in list(prefetch_product_size.productsize_set.filter(skin_type_id=1)|prefetch_product_size.productsize_set.filter(skin_type_id=None))]
+                product_size_data       = [{
+                    'product_name'     : product.name,
+                    'product_size'     : product_size.size.name,
+                    'product_price'    : f'{int(product_size.price):,}',
+                    'size_description' : product_size.size.description
+                    } for product_size
+                in list(prefetch_product_size.productsize_set.filter(skin_type_id=1)|prefetch_product_size.productsize_set.filter(skin_type_id=None))]
                 return JsonResponse({'Info':product_size_data},status=200)
 
         except Product.DoesNotExist:
